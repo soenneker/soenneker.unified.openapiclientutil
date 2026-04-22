b2b5944715f3e437639f59b9b4bdbb04872291e5
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:48 -0500

    TUnit conversion

--- test/Soenneker.Unified.OpenApiClientUtil.Tests/UnifiedOpenApiClientUtilTests.cs
+++ test/Soenneker.Unified.OpenApiClientUtil.Tests/UnifiedOpenApiClientUtilTests.cs
@@ -1,20 +1,19 @@
 using Soenneker.Unified.OpenApiClientUtil.Abstract;
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Unified.OpenApiClientUtil.Tests;
 
-[Collection("Collection")]
-public sealed class UnifiedOpenApiClientUtilTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class UnifiedOpenApiClientUtilTests : HostedUnitTest
 {
     private readonly IUnifiedOpenApiClientUtil _openapiclientutil;
 
-    public UnifiedOpenApiClientUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public UnifiedOpenApiClientUtilTests(Host host) : base(host)
     {
         _openapiclientutil = Resolve<IUnifiedOpenApiClientUtil>(true);
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
